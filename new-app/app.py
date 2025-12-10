98fa23429c21147f00e453dcecaaffa4d17c504f
FatenAish <fatenaish199f@gmail.com>
2025-12-10 11:41:52 +0300

    Create app.py

---
+++ new-app/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="New Test App", layout="wide")
+
+st.title("🎉 New Test App is Running!")
+st.write("If you see this message on Cloud Run, everything works perfectly.")
